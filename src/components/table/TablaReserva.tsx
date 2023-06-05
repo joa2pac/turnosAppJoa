bc5c0c977e86efab10410139347267d8a850f510
joaquin <joaquinahuel93@gmail.com>
2023-06-05 12:22:23 -0300

    cambio boton confirmado usuario

--- src/components/table/TablaReserva.tsx
+++ src/components/table/TablaReserva.tsx
@@ -132,12 +132,12 @@ export const TablaReserva = () => {
                 disabled={reserva.reservados === reserva.limiteReserva || isReservado(reserva)}
                 type="button"
                 className={`text-white ${
-                  reserva.reservados === reserva.limiteReserva || isReservado(reserva)
+                  reserva.reservados === reserva.limiteReserva
                     ? "bg-gray-500"
                     : "bg-blue-700 hover:bg-blue-800"
-                }   focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
-                data-modal-target="modalId"
-                data-modal-toggle="popup-modal"
+                } ${
+                  isReservado(reserva) ? "bg-green-500" : "bg-blue-700 hover:bg-blue-800"
+                }  focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
               >
                 Reservá
               </button>
